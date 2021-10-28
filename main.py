e4e4d197a4f8c4def8dfa7c48d2f5e467be3a30f
Patryk_Kulpinski <61681867+KulpinskiPatryk@users.noreply.github.com>
2021-10-28 09:38:37 +0200

    Upgrade

--- main.py
+++ main.py
@@ -1,85 +1,119 @@
-from numpy.random import choice as law_of_nature
+import numpy.random
+from numpy.random import choice as law
 from numpy.random import randint as randint
 import random
 
-god_pop = []
-used_values = []
-val_Pop = 4
-iteration = 3
-a = 1
-b = 1
-c = 1
+
+# Translacja z systemu dwójkowego na system dziesiątkowy
+def translate(v):
+    number = ''
+    for i in range(len(v)):
+        number = number + str(v[i])
+    return int(number, 2)
 
 
-
-def best():
-    winner = 0
-    for v in range(val_Pop):
-        if winner < translate(v):
-            winner = translate(v)
-    return winner
-
-
-def fitness(x):
+# Funkcja Przystosowania czyli funkcja kwadratowa
+def fitness(x, a, b, c):
     return a * x ** 2 + b * x + c
 
 
+# Krzyżowanie Kopjujemy wartości by utworzyć pomocnicze tablice dzielimy je i wpisujemy do początkowych tablic
+# by utworzyć dzieci
+def crossover(r1, r2, cross_point):
+    p1 = r1.copy()
+    p2 = r2.copy()
+    pt = randint(1, len(p1))
+    if numpy.random.rand() < cross_point:
+        r1 = p1[:pt] + p2[pt:]
+        r2 = p2[:pt] + p1[pt:]
+    return r1, r2
+
+
 # Mutacja
-def mut(r):
+def mut(r, mutation_point):
     for i in range(8):
-        if ((random.randint(0, 9) / 10) <= 0.1):
-            if (god_pop[r][i] > 0):
-                god_pop[r][i] = 0
+        if numpy.random.rand() < mutation_point:
+            if r[i] > 0:
+                r[i] = 0
             else:
-                god_pop[r][i] = 1
+                r[i] = 1
 
 
-# Krzyżowanie
-def crossover(r1, r2):
-    p1 = god_pop[r1].copy()
-    p2 = god_pop[r2].copy()
-    pt = randint(1, len(p1) - 2)
-    if random.randint(0, 11) <= 7:
-        god_pop[r1] = p1[:pt] + p2[pt:]
-        god_pop[r2] = p2[:pt] + p1[pt:]
-    mut(r1)
-    mut(r2)
+# Poszukiwanie najlepszego osobnika wyniku
+def best_specimen(pop, a, b, c):
+    # Utworzenie listy z przystosowaniem
+    pop_fitneses = []
+    for sub in range(n_pop):
+        pop_fitneses.append(fitness(translate(pop[sub]), a, b, c))
+    og = translate(pop[0])
+    og_val = fitness(og, a, b, c)
+    # Znalezienie najlepszego osobnika
+    for sub in range(n_pop):
+        if pop_fitneses[sub] > og:
+            og = translate(pop[sub])
+            og_val = pop_fitneses[sub]
+    return og, og_val
 
 
-# Translacja
-def translate(v):
-    liczba = ''
-    for i in range(len(god_pop[v])):
-        liczba = liczba + str(god_pop[v][i])
-    return int(liczba, 2)
+def genetic_algorithm(iteration, n_pop, cross_point, mutation_point, a, b, c):
+    # Utworzenie populacji początkowej
+    pop = []
+    for i in range(n_pop):
+        pop.append([])
+    for i in pop:
+        for v in range(8):
+            i.append(random.randint(0, 1))
+    numbered_list = []
+    for _ in range(n_pop):
+        numbered_list.append(_)
+    # Wykożystanie pierwszego osobnika jako najlepszego
+    best = translate(pop[0])
+    best_val = fitness(best, a, b, c)
+    print('f(' + str(best) + ') = ' + str(best_val))
+    print('Działanie Programu: ')
+    # Rozpoczęcie algorytmu
+    for gen in range(iteration):
+        # Całkowita suma przystosowania
+        pop_fitness = 0
+        for sub in range(n_pop):
+            pop_fitness = pop_fitness + fitness(translate(pop[sub]), a, b, c)
+        # Utworzenie listy z poziomem przystosowania
+        pop_probabilities = []
+        for sub in range(n_pop):
+            pop_probabilities.append(fitness(translate(pop[sub]), a, b, c) / pop_fitness)
+        # Utworzenie listy z przystosowaniem
+        pop_fitneses = []
+        for sub in range(n_pop):
+            pop_fitneses.append(fitness(translate(pop[sub]), a, b, c))
+        # Znalezienie najlepszego osobnika
+        for sub in range(n_pop):
+            if pop_fitneses[sub] > best_val:
+                best = translate(pop[sub])
+                best_val = pop_fitneses[sub]
+        selected = []
+        for sub in range(n_pop):
+            selected.append(law(numbered_list, p=pop_probabilities))
+        children = []
+        for i in range(0, n_pop, 2):
+            p1, p2 = pop[selected[i]], pop[selected[i + 1]]
+            p1, p2 = crossover(p1, p2, cross_point)
+            mut(p1, mutation_point)
+            mut(p2, mutation_point)
+            children.append(p1)
+            children.append(p2)
+        pop = children.copy()
+        print(pop)
+    over_best, over_score = best_specimen(pop, a, b, c)
+    return over_best, over_score
 
 
-def selection():
-    r1 = random.randint(0, val_Pop - 1)
-    return r1
-
-
-def roulette_wheel_selection():
-    # Computes the totallity of the population fitness
-    god_pop_fitness = sum([fitness(translate(pop)) for pop in range(len(god_pop))])
-    print(god_pop_fitness)
-    # Computes for each chromosome the probability
-    pop_probabilities = [fitness(translate(pop)) / god_pop_fitness for pop in range(len(god_pop))]
-    print(pop_probabilities)
-    # Selects one chromosome based on the computed probabilities
-    #return law_of_nature(god_pop, p=pop_probabilities)
-
-
-for i in range(val_Pop):
-    god_pop.append([])
-for i in god_pop:
-    for v in range(8):
-        i.append(random.randint(0, 1))
-print(god_pop)
-print(best())
-for i in range(iteration):
-    for v in range(int(val_Pop)):
-        crossover(selection(), selection())
-    print(god_pop)
-    print(best())
-    roulette_wheel_selection()
+n_pop = 2
+n_int = 2
+cross_point = 0.9
+mutation_point = 0.1
+a = 1
+b = 1
+c = 1
+best, score = genetic_algorithm(n_int, n_pop, cross_point, mutation_point, a, b, c)
+print("Wynik : ")
+print('f(' + str(best) + ') = ' + str(score))
